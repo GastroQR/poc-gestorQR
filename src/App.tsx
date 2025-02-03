3c2cece50db12aedcb8f3a85ba9db148c00b7af2
S4ntifdz <safernandez@frro.utn.edu.ar>
2025-02-03 20:10:46 -0300

    fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Demo from './pages/Demo';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/poc-gestorQR/demo">
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/table/:tableId" element={<ClientView />} />
